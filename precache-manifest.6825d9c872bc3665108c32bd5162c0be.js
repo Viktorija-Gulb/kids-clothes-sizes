8e0002dabb822ec6f6a17c99d2caa8356397388b
vikgulb <vikgulb@gmail.com>
2020-03-25 16:20:55 +0200

    Updates

---
+++ precache-manifest.6825d9c872bc3665108c32bd5162c0be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e948efd1f20cb1782ce416fc89b7f882",
+    "url": "/kids-clothes-sizes/index.html"
+  },
+  {
+    "revision": "5b6b8c4aa87c0cabcaf0",
+    "url": "/kids-clothes-sizes/static/css/main.2f71ace9.chunk.css"
+  },
+  {
+    "revision": "d57af6e433f5893c7488",
+    "url": "/kids-clothes-sizes/static/js/2.f4a4ac3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kids-clothes-sizes/static/js/2.f4a4ac3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6b8c4aa87c0cabcaf0",
+    "url": "/kids-clothes-sizes/static/js/main.3b8ba584.chunk.js"
+  },
+  {
+    "revision": "d20b77d10032aa916a82",
+    "url": "/kids-clothes-sizes/static/js/runtime-main.6639508f.js"
+  },
+  {
+    "revision": "41fa2fbd7278f5fc84bd87749817f4c8",
+    "url": "/kids-clothes-sizes/static/media/ben-white.41fa2fbd.jpg"
+  },
+  {
+    "revision": "fe8a9a166f62cbbf2a6b81f74e4abd19",
+    "url": "/kids-clothes-sizes/static/media/children.fe8a9a16.jpg"
+  },
+  {
+    "revision": "d3aabcfa3abd3dc263f1fd6943afbcbd",
+    "url": "/kids-clothes-sizes/static/media/felipe-salgado.d3aabcfa.jpg"
+  },
+  {
+    "revision": "b1a30e39c206857e8ddc3e61b1a4f3ff",
+    "url": "/kids-clothes-sizes/static/media/jonathan-borba.b1a30e39.jpg"
+  },
+  {
+    "revision": "d077995eff83a969c350861c88adee12",
+    "url": "/kids-clothes-sizes/static/media/ryan-plomp.d077995e.jpg"
+  },
+  {
+    "revision": "08506a476d1157d4fc666a1b7740dfc9",
+    "url": "/kids-clothes-sizes/static/media/travis-grossen.08506a47.jpg"
+  }
+]);
