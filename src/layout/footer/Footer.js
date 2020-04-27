7c0fc116bf7a2a25669745a15c3f5d4c770e7276
vikgulb <vikgulb@gmail.com>
2020-04-27 17:21:41 +0300

    fix bug in footer greeting

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,29 +1,29 @@
 import React from "react";
-import './footer.scss';
+import "./footer.scss";
 
 function Footer() {
-
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
 
-  if(hours < 12) {
-    timeOfDay = "Gražaus jums ryto!"
-  } else if (hours >=12 && hours < 17) {
-    timeOfDay = "Gražios dienos!"
-  } else if (hours >= 18 && hours < 21){
-    timeOfDay = "Gražaus vakaro!"
+  if (hours < 12) {
+    timeOfDay = "Gražaus jums ryto!";
+  } else if (hours >= 12 && hours < 18) {
+    timeOfDay = "Gražios dienos!";
+  } else if (hours >= 18 && hours < 21) {
+    timeOfDay = "Gražaus vakaro!";
   } else {
-    timeOfDay = "Labos nakties!"
+    timeOfDay = "Labos nakties!";
   }
 
-  return(
+  return (
     <footer className="footer">
       <p className="footer__time-of-day">{timeOfDay}</p>
-      <span className="footer__copyright">&copy; {new Date().getFullYear()} ViArt</span>
+      <span className="footer__copyright">
+        &copy; {new Date().getFullYear()} ViArt
+      </span>
     </footer>
   );
 }
-
 
 export default Footer;
